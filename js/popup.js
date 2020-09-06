52b97048196ff361dfbe129da9292f2c277c0750
Pierluigi Zagaria <pierluigizagaria@gmail.com>
2020-09-06 09:08:09 +0200

    Migrated to typewriterjs library for text animation.

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,6 @@
 let texts = ["what's up?","What's up?"]
-let lastWrite
 
-function typeWriter(selector, text, callback) {
-    let i = 0
-    if (lastWrite) clearInterval(lastWrite)
-    lastWrite = setInterval(() => {
-        if (i < (text.length)) {
-            document.querySelector(selector).innerHTML = text.substring(0, ++i) + '<span class="text-cursor" aria-hidden="true"></span>';
-        } else if (typeof fnCallback == 'function') {
-            clearInterval(lastWrite)
-            callback()
-        }
-    }, 50)
-}
+var typewriter = new Typewriter(document.getElementById('text'));
 
 function toggle() {
     let state = document.querySelector("input").checked
@@ -21,12 +9,13 @@ function toggle() {
 }
 
 function showText(state){
-    typeWriter('#text', state ? texts[1] : texts[0])
+    new Typewriter(document.getElementById('text'), {delay: 50})
+        .typeString(state ? texts[1] : texts[0])
+        .start()
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('input').addEventListener('click', toggle);
-
     chrome.storage.sync.get(['enabled'], (options) => {
         showText(options.enabled)
         document.querySelector('input').checked = options.enabled
