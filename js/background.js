4546772934f1cdccae1dace6e5e7377910324aab
Pierluigi Zagaria <pierluigizagaria@gmail.com>
2023-04-23 21:51:53 +0200

    Updated to manifest v3

--- js/background.js
+++ js/background.js
@@ -1,12 +1,28 @@
+chrome.runtime.onStartup.addListener(function() { 
+  chrome.action.disable(); 
+});
+
 chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.disable();  
     chrome.storage.sync.set({
         enabled:  true
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+      chrome.declarativeContent.onPageChanged.addRules([
+        {
+          conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { hostSuffix: "web.whatsapp.com" },
+            }),
+          ],
+          actions: [new chrome.declarativeContent.ShowAction()],
+        },
+      ]);
     });
 });
 
 chrome.runtime.onMessage.addListener((message, sender) => {
     if (typeof message === 'object' && message.type === 'wake') {
-        chrome.pageAction.show(sender.tab.id);
         chrome.storage.sync.get(['enabled'], (options) => {
             chrome.tabs.sendMessage(sender.tab.id, { type: 'changeState', enabled: options.enabled })
         }) 
