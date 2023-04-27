3a04b14b9df526228123e79049c86744ef5df851
Pierluigi Zagaria <pierluigizagaria@gmail.com>
2023-04-27 10:21:46 +0200

    Fixed CTRL shortcuts

--- js/content.js
+++ js/content.js
@@ -1,15 +1,16 @@
-chrome.runtime.sendMessage({ type: 'wake' });
-chrome.runtime.onMessage.addListener(message => {
-    if (message.type == 'changeState') enabled = message.enabled
-});
+let enabled = false;
 
-let enabled;
+chrome.runtime.sendMessage({ type: 'wake' });
+
+chrome.runtime.onMessage.addListener(message => {
+    if (message.type == 'changeState') enabled = message.enabled;
+});
 
 document.addEventListener('keydown', (event) => {
     let input = event.target
-    let text = input.innerText
-    if (enabled && text.replace(/\s/g, '').length === 0 && event.key.length === 1) {
+    let text = input.innerText.replace(/\s/g, '');
+    if (enabled && !event.ctrlKey && text.length === 0 && event.key.length === 1) {
         document.execCommand('insertText', input, event.key.toUpperCase());
-        event.preventDefault()
+        event.preventDefault();
     }
 }, { passive: false });
